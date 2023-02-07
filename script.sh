a0acdfd3b2ce4fc60de8b629412c83bc9709aeaf
Jan <jan@amperecomputing.com>
2023-02-07 19:26:27 +0000

    wip

---
+++ script.sh
@@ -0,0 +1,2 @@
+python3 scripts/txt2img.py --prompt "a professional photo
+graph of an astronaut riding a horse" --ckpt stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt --config configs/stable-diffusion/intel/v2-inference-fp32.yaml --torchscript
