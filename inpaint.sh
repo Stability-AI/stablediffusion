39b25fe7505adbaa6f6ee1f40f2737675dd864ed
Yewon Lim <ga06033@yonsei.ac.kr>
2023-07-06 04:44:19 +0000

    230706 | inpainted imgs

---
+++ inpaint.sh
@@ -0,0 +1,5 @@
+python scripts/inpaint.py --src inpaintng_Birman_9.jpg\
+                          --mask inpaintmask_Birman_9.jpg\
+                          --prompt "A cat is sitting on the TV"\
+                          --steps 100\
+                          --n_sample 10
